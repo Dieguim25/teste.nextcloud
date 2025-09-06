3b4a2f517a1e394f160e13c9ba6c4f7f66855ee1
Diego Zeppe <163900835+Dieguim25@users.noreply.github.com>
2025-09-05 21:47:04 -0300

    Update update.sh

--- static/update.sh
+++ static/update.sh
@@ -5,7 +5,7 @@
 true
 SCRIPT_NAME="Update Server + Nextcloud"
 # shellcheck source=lib.sh
-source /var/script/lib.sh
+source <(curl -sL https://raw.githubusercontent.com/Dieguim25/vm/refs/heads/main/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
